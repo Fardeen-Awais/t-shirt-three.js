5aa495e3069596d519f57dc4bee87e854310bc93
fardeen awais <fardeenjutt99@gmail.com>
2023-06-13 15:57:05 +0500

    Adding hero text

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Nunito } from 'next/font/google'
 
+const nunito = Nunito({subsets: ['latin']})
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -11,7 +13,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${nunito.className}`}>{children}</body>
     </html>
   )
 }
+
